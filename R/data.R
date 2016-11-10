785e3e0b375238fda4257191b171dd49a677d17c
Jarad Niemi <niemi@iastate.edu>
2016-11-10 15:58:29 -0600

    Add all survey data

--- R/data.R
+++ R/data.R
@@ -1,11 +1,31 @@
 #' STRIPS Survey data
 #'
-#' STRIPS Survey data for questions 3 and 15
+#' STRIPS Survey data for all questions. At the moment, this data set does not 
+#' read in NA's properly and therefore caution should be used when looking at 
+#' questions other than Q3 and Q15
 #'
-#' @format A data.frame with 18 variables the first of which is \code{CaseID} 
-#' and the remainder are survey questions. 
+#' @format A data.frame with a lot of variables the first of which is 
+#' \code{CaseID} and the remaining 123 columns are survey questions. 
 #'
-#' @examples
-#' survey
-"survey"
+#' @author J. Gordon Arbuckle, \email{arbuckle@iastate.edu}
+#'
+#' @seealso \link{\code{survey_questions}}
+#'
+"survey_responses"
+
+
+#' STRIPS Survey questions
+#'
+#' To aid analysis, survey data are recorded using the question number and this
+#' data.frame provides the associated wording for the question. 
+#'
+#' @format A data.frame with a 2 variables: \code{question} and 
+#' \code{question_text}.
+#'
+#' @author J. Gordon Arbuckle, \email{arbuckle@iastate.edu}
+#'
+#' @seealso \link{\code{survey_responses}}
+#'
+"survey_questions"
+
 
--- data-raw/survey_responses.R
+++ data-raw/survey_responses.R
@@ -1,13 +1,13 @@
 library(dplyr)
 
-survey <- readr::read_csv("Neal_Smith_Survey_Data19May14.dat",
+survey_responses <- readr::read_csv("Neal_Smith_Survey_Data19May14.dat",
                           na = c("8","9")) %>%                 # This is likely not correct for columns other than Q3 and Q15
   dplyr::select(c(1:8,40:101,107:124))                         # Remove columns for a variety of reasons
                          
 
-names(survey) <- gsub("\\.(.*)", "", names(survey))
+names(survey_responses) <- gsub("\\.(.*)", "", names(survey_responses))
 
-devtools::use_data(survey, overwrite = TRUE)
+devtools::use_data(survey_responses, overwrite = TRUE)
 
 # Simplify the presentation by using just using the question number for column
 # names and then providing another data set that has the actual question.
